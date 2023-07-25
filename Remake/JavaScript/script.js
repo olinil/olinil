beb65076d1e48e45683ff38e291b546e1f6d8424
Oliver fra bergen <e-olinil@asvg.no>
2023-07-25 02:24:38 +0200

    Brand new design

--- Remake/JavaScript/script.js
+++ Remake/JavaScript/script.js
@@ -1,21 +1,39 @@
-const animatedText = document.getElementById("welcome-text");
-let dots = ".";
+const changingText = document.getElementById("changing-text");
+let currentTitleIndex = 0;
+
+const titles = ["IT operations technician", "IT Profetional", "Aprentice", "App developer", "Web designer", "Server manager", "Programmer", "Network engineer"];
 
 function animateText() {
-  animatedText.textContent = "Welcome" + dots;
+  changingText.textContent = titles[currentTitleIndex];
+  currentTitleIndex = (currentTitleIndex + 1) % titles.length;
 
-  dots += ".";
-
-  if (dots.length > 3) {
-    dots = ".";
-  }
-
-  setTimeout(animateText, 500); // Adjust the interval time (in milliseconds) to control the speed of animation
+  setTimeout(animateText, 1000); // Change the interval time (in milliseconds) to control the speed of animation
 }
+
+
+
+
+
+// Get the modal element
+const modal = document.getElementById("myModal");
+
+// Function to open the popup
+function openPopup() {
+  modal.style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  modal.style.display = "none";
+}
+
+
+
+
+
 
 // Start the animation
 animateText();
-
 
 function hideVideo() {
     const videoContainer = document.getElementById("video-container");
@@ -30,3 +48,4 @@ function setVideoSpeed(speed) {
 
 // Example usage: Change speed to 1.5x
 setVideoSpeed(1.5);
+
