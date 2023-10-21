79f83a81aebe9af163f945182434018d3ca2f85c
Oliver fra bergen <71375194+olinil@users.noreply.github.com>
2023-10-21 17:00:45 +0200

    V4.02.09 - Z-Index change & overflow hidden

--- JavaScript/loading.js
+++ JavaScript/loading.js
@@ -2,3 +2,8 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+
+var noScroll = document.getElementById("body");
+window.addEventListener("load", function(){
+    noScroll.style.overflow = "hidden";
+})
