c8f63ec8c5122479fb48358a6586cb369af6fe37
Oliver fra bergen <71375194+olinil@users.noreply.github.com>
2023-10-21 17:21:11 +0200

    Update loading.js

--- JavaScript/loading.js
+++ JavaScript/loading.js
@@ -1,9 +1,9 @@
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
-})
+});
 
 var noScroll = document.getElementById("body");
 window.addEventListener("load", function(){
     noScroll.style.overflow = "hidden";
-})
+});
